f52a3e020866a19c22edf23f691cbea564e33fcd
Paul Ortman <paul.ortman@gmail.com>
2017-05-17 22:03:40 -0500

    new approach wip

--- test.py
+++ test.py
@@ -30,12 +30,42 @@ def test_major_updates_only_1():
     assert('2.0.0' in subset)
 
 def test_major_updates_only_2():
-    mask = 'Y.0.0'
+    mask = 'Y.0.0' # tell me major version changes only once per major version
     versions = ['1.8.0', '1.8.1', '1.8.2', '1.9.0', '1.9.1', '1.10.0', '2.0.1', '2.0.2']
     current_version = '1.9.0'
     subset = VersionFilter.semver_filter(mask, versions, current_version)
-    assert(1 == len(subset))
-    assert('2.0.1' in subset)
+    assert(0 == len(subset))
+
+def test_minor_updates_1():
+    mask = 'Y.Y.0' # tell me minor version changes only once per minor version, exclude all patch updates
+    versions = ['1.8.0', '1.8.1', '1.8.2', '1.9.0', '1.9.1', '1.10.0', '2.0.0', '2.0.1']
+    current_version = '1.8.0'
+    subset = VersionFilter.semver_filter(mask, versions, current_version)
+    assert(3 == len(subset))
+    assert('1.9.0' in subset)
+    assert('1.10.0' in subset)
+    assert('2.0.0' in subset)
+
+def test_minor_updates_2():
+    mask = 'L.Y.0' # give me minor version changes only for my current major version, exclude all patch updates
+    versions = ['1.8.0', '1.8.1', '1.8.2', '1.9.0', '1.9.1', '1.10.0', '2.0.0', '2.0.1']
+    current_version = '1.8.0'
+    subset = VersionFilter.semver_filter(mask, versions, current_version)
+    assert(3 == len(subset))
+    assert('1.9.0' in subset)
+    assert('1.10.0' in subset)
+    assert('2.0.0' in subset)
+
+def test_all_updates_1():
+    mask = 'Y.Y.Y' # Give me every patch, minor and major update
+    versions = ['1.8.0', '1.8.1', '1.8.2', '1.9.0', '1.9.1', '1.10.0', '2.0.0', '2.0.1']
+    current_version = '1.8.0'
+    subset = VersionFilter.semver_filter(mask, versions, current_version)
+    assert(3 == len(subset))
+    assert('1.9.0' in subset)
+    assert('1.10.0' in subset)
+    assert('2.0.0' in subset)
+
 
 def test_explicit_major_updates_only_1():
     mask = '2.0.0'
--- version_filter.py
+++ version_filter.py
@@ -11,7 +11,7 @@ class VersionFilter(object):
     def semver_filter(mask, versions, current_version):
         """Return a list of versions that are greater than the current version and that match the mask"""
         current = semantic_version.Version(current_version)
-        mask = VersionMask(mask)
+        _mask = VersionMask(mask)
 
         _versions = []
         for version in versions:
@@ -22,51 +22,79 @@ class VersionFilter(object):
             _versions.append(v)
         _versions.sort()
 
-        selected_versions = []
-        for v in _versions:
-            if current >= v:
-                continue  # skip all versions 'less' than the current version
+        if _mask.major == LOCK:
+            _mask.major = current.major
+        if _mask.minor == LOCK:
+            _mask.minor = current.minor
+        if _mask.patch == LOCK:
+            _mask.patch = current.patch
 
-            if mask.major == LOCK and v.major != current.major:
-                continue  # skip all versions who's major is not locked to the current version
+        if 'Y' in mask:
+            index = mask.index('Y')
+        spec = semantic_version.Spec('{},>{}'.format(mask, current_version))
+        selected_versions = [v for v in _versions if v in spec]
 
-            if mask.minor == LOCK and v.minor != current.minor:
-                continue  # skip all versions who's minor is not locked to the current version
+        if 'Y' not in mask:
+            # if there is no YES in the mask the library should have gotten what we wanted
+            return [str(v) for v in selected_versions]
 
-            if mask.patch == LOCK and v.patch != current.patch:
-                continue  # skip all versions who's patch is not locked to the current version
+        # if there is at least one YES in the mask, use the rightmost YES to determine the 'precision' of versions to report
+        else:
+            spec = semantic_version.Spec('>{}'.format(current_version))
+            selected_versions = [v for v in _versions if v in spec]
+            if _mask.patch == YES:
+                pass # they want everything
+            if _mask.minor == YES and _mask.patch != YES:
+                selected_versions = [x for x in selected_versions if x.patch == 0]
+            if _mask.major == YES and _mask.minor != YES and _mask.patch != YES:
+                selected_versions = [x for x in selected_versions if x.minor == 0 and x.patch == 0]
+            return [str(v) for v in selected_versions]
 
-            selected_versions.append(v)
-
-        major_versions = []
-        if mask.major == YES:
-            major_slots = []
-            for version in selected_versions:
-                trunc_version = str(version.major)
-                if version.major > current.major and trunc_version not in major_slots:
-                    major_slots.append(trunc_version)
-                    major_versions.append(version)
-
-        minor_versions = []
-        if mask.minor == YES:
-            minor_slots = []
-            for version in selected_versions:
-                trunc_version = '{}.{}'.format(version.major, version.minor)
-                if version.minor > current.minor and trunc_version not in minor_slots:
-                    minor_slots.append(trunc_version)
-                    minor_versions.append(version)
-
-        patch_versions = []
-        if mask.patch == YES:
-            patch_slots = []
-            for version in selected_versions:
-                trunc_version = '{}.{}.{}'.format(version.major, version.minor, version.patch)
-                if version.patch > current.patch and trunc_version not in patch_slots:
-                    patch_slots.append(trunc_version)
-                    patch_versions.append(version)
-
-        if mask.major == YES or mask.minor == YES or mask.patch == YES:
-            selected_versions = sorted(list(set(major_versions + minor_versions + patch_versions)))
+        # selected_versions = []
+        # for v in _versions:
+        #     if current >= v:
+        #         continue  # skip all versions 'less' than the current version
+        #
+        #     if mask.major == LOCK and v.major != current.major:
+        #         continue  # skip all versions who's major is not locked to the current version
+        #
+        #     if mask.minor == LOCK and v.minor != current.minor:
+        #         continue  # skip all versions who's minor is not locked to the current version
+        #
+        #     if mask.patch == LOCK and v.patch != current.patch:
+        #         continue  # skip all versions who's patch is not locked to the current version
+        #
+        #     selected_versions.append(v)
+        #
+        # major_versions = []
+        # if mask.major == YES:
+        #     major_slots = []
+        #     for version in selected_versions:
+        #         trunc_version = str(version.major)
+        #         if version.major > current.major and trunc_version not in major_slots:
+        #             major_slots.append(trunc_version)
+        #             major_versions.append(version)
+        #
+        # minor_versions = []
+        # if mask.minor == YES:
+        #     minor_slots = []
+        #     for version in selected_versions:
+        #         trunc_version = '{}.{}'.format(version.major, version.minor)
+        #         if version.minor > current.minor and trunc_version not in minor_slots:
+        #             minor_slots.append(trunc_version)
+        #             minor_versions.append(version)
+        #
+        # patch_versions = []
+        # if mask.patch == YES:
+        #     patch_slots = []
+        #     for version in selected_versions:
+        #         trunc_version = '{}.{}.{}'.format(version.major, version.minor, version.patch)
+        #         if version.patch > current.patch and trunc_version not in patch_slots:
+        #             patch_slots.append(trunc_version)
+        #             patch_versions.append(version)
+        #
+        # if mask.major == YES or mask.minor == YES or mask.patch == YES:
+        #     selected_versions = sorted(list(set(major_versions + minor_versions + patch_versions)))
 
 
         return [str(v) for v in selected_versions]
@@ -93,4 +121,5 @@ class VersionMask(object):
             raise ValueError
 
     def __str__(self):
-        return self.str
+        # return self.str
+        return '.'.join(str(x) for x in [self.major, self.minor, self.patch] if x != '')
