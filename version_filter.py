792a218d3c26d7786bad494c5c769fb29e7d0be5
Paul Ortman <paul.ortman@gmail.com>
2017-05-18 22:34:17 -0500

    much is working, but wip

--- version_filter.py
+++ version_filter.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 import re
+import operator
 import semantic_version
 
 LOCK = 'L'
@@ -8,10 +9,10 @@ YES = 'Y'
 class VersionFilter(object):
 
     @staticmethod
-    def semver_filter(mask, versions, current_version):
+    def semver_filter(mask, versions, current_version=None):
         """Return a list of versions that are greater than the current version and that match the mask"""
-        current = semantic_version.Version(current_version)
-        _mask = VersionMask(mask)
+        current = semantic_version.Version(current_version) if current_version else None
+        _mask = SpecMask(mask, current)
 
         _versions = []
         for version in versions:
@@ -22,80 +23,7 @@ class VersionFilter(object):
             _versions.append(v)
         _versions.sort()
 
-        if _mask.major == LOCK:
-            _mask.major = current.major
-        if _mask.minor == LOCK:
-            _mask.minor = current.minor
-        if _mask.patch == LOCK:
-            _mask.patch = current.patch
-
-        if 'Y' in mask:
-            index = mask.index('Y')
-        spec = semantic_version.Spec('{},>{}'.format(mask, current_version))
-        selected_versions = [v for v in _versions if v in spec]
-
-        if 'Y' not in mask:
-            # if there is no YES in the mask the library should have gotten what we wanted
-            return [str(v) for v in selected_versions]
-
-        # if there is at least one YES in the mask, use the rightmost YES to determine the 'precision' of versions to report
-        else:
-            spec = semantic_version.Spec('>{}'.format(current_version))
-            selected_versions = [v for v in _versions if v in spec]
-            if _mask.patch == YES:
-                pass # they want everything
-            if _mask.minor == YES and _mask.patch != YES:
-                selected_versions = [x for x in selected_versions if x.patch == 0]
-            if _mask.major == YES and _mask.minor != YES and _mask.patch != YES:
-                selected_versions = [x for x in selected_versions if x.minor == 0 and x.patch == 0]
-            return [str(v) for v in selected_versions]
-
-        # selected_versions = []
-        # for v in _versions:
-        #     if current >= v:
-        #         continue  # skip all versions 'less' than the current version
-        #
-        #     if mask.major == LOCK and v.major != current.major:
-        #         continue  # skip all versions who's major is not locked to the current version
-        #
-        #     if mask.minor == LOCK and v.minor != current.minor:
-        #         continue  # skip all versions who's minor is not locked to the current version
-        #
-        #     if mask.patch == LOCK and v.patch != current.patch:
-        #         continue  # skip all versions who's patch is not locked to the current version
-        #
-        #     selected_versions.append(v)
-        #
-        # major_versions = []
-        # if mask.major == YES:
-        #     major_slots = []
-        #     for version in selected_versions:
-        #         trunc_version = str(version.major)
-        #         if version.major > current.major and trunc_version not in major_slots:
-        #             major_slots.append(trunc_version)
-        #             major_versions.append(version)
-        #
-        # minor_versions = []
-        # if mask.minor == YES:
-        #     minor_slots = []
-        #     for version in selected_versions:
-        #         trunc_version = '{}.{}'.format(version.major, version.minor)
-        #         if version.minor > current.minor and trunc_version not in minor_slots:
-        #             minor_slots.append(trunc_version)
-        #             minor_versions.append(version)
-        #
-        # patch_versions = []
-        # if mask.patch == YES:
-        #     patch_slots = []
-        #     for version in selected_versions:
-        #         trunc_version = '{}.{}.{}'.format(version.major, version.minor, version.patch)
-        #         if version.patch > current.patch and trunc_version not in patch_slots:
-        #             patch_slots.append(trunc_version)
-        #             patch_versions.append(version)
-        #
-        # if mask.major == YES or mask.minor == YES or mask.patch == YES:
-        #     selected_versions = sorted(list(set(major_versions + minor_versions + patch_versions)))
-
+        selected_versions = [v for v in _versions if v in _mask]
 
         return [str(v) for v in selected_versions]
 
@@ -137,7 +65,7 @@ class SpecItemMask(object):
 
         match = self.re_specitemmask.match(specitemmask)
         if not match:
-            raise ValueError('Invalid SpecItemMask: {}'.format(specitemmask))
+            raise ValueError('Invalid SpecItemMask: "{}"'.format(specitemmask))
 
         kind, version = match.groups()
         if self.YES in version:
@@ -172,30 +100,86 @@ class SpecItemMask(object):
 
         return kind, version
 
+    def match(self, version):
+        spec_match = version in self.spec
+        if not self.has_yes:
+            return spec_match
+        else:
+            # if it had a YES flag, we must additionally make sure the version has zeros for each position
+            # 'less significant' than the YES position
+            if self.yes_pos == self.MAJOR:
+                return spec_match and version.minor == 0 and version.patch == 0
+            if self.yes_pos == self.MINOR:
+                return spec_match  and version.patch == 0
+            if self.yes_pos == self.PATCH:
+                return spec_match
+
+    def __contains__(self, item):
+        return self.match(item)
+
 
     def get_spec(self):
         return semantic_version.Spec("{}{}".format(self.kind, self.version))
 
 
 class SpecMask(object):
+    AND = "&&"
+    OR = "||"
+
     def __init__(self, specmask, current_version=None):
         self.speckmask = specmask
         self.current_version = current_version
+        self.specs = None
+        self.op = None
         self.parse(specmask)
-        all_specs = self.specs_ands + self.specs_ors
 
         # if any of the sp
-        if any([s.has_lock for s in all_specs]) and not current_version:
-            raise ValueError('SpecMask must be given a current_version if LOCKs are specified')
+        # if any([s.has_lock for s in self.specs]) and not current_version:
+        #     raise ValueError('SpecMask must be given a current_version if LOCKs are specified')
 
     def parse(self, specmask):
-        self.specs_ors = [x.strip() for x in specmask.split("||")]
-        self.specs_ands = [x.strip() for x in specmask.split("&&")]
+        if self.OR in specmask and self.AND in specmask:
+            raise ValueError('SpecMask cannot contain both {} and {} operators'.format(self.OR, self.AND))
 
+        if self.OR in specmask:
+            self.op = self.OR
+            self.specs = [x.strip() for x in specmask.split(self.OR)]
+        elif self.AND in specmask:
+            self.op = self.AND
+            self.specs = [x.strip() for x in specmask.split(self.AND)]
+        else:
+            self.op = self.AND
+            self.specs = [specmask.strip(),]
 
-    def test(self, version):
-        pass
+        self.specs = [SpecItemMask(s, self.current_version) for s in self.specs]
+
+    def match(self, version):
+        if isinstance(version, str):
+            v = semantic_version.Version(version)
+        elif isinstance(version, semantic_version.Version):
+            v = version
+        else:
+            raise ValueError('version must be either a str or a Version object')
+
+        # We implicitly require that SpecMasks disregard releases older than the current_version if it is specified
+        if self.current_version:
+            newer_than_current = semantic_version.Spec('>{}'.format(self.current_version))
+        else:
+            newer_than_current = semantic_version.Spec('*')
+
+        if self.op == self.AND:
+            return all([v in x for x in self.specs]) and v in newer_than_current
+        else:
+            return any([v in x for x in self.specs]) and v in newer_than_current
+
+    def __contains__(self, item):
+        return self.match(item)
+
+    def __eq__(self, other):
+        if not isinstance(other, SpecMask):
+            return NotImplemented
+
+        return set(self.specs) == set(other.specs)
 
     def __str__(self):
-        # return self.str
-        return '.'.join(str(x) for x in [self.major, self.minor, self.patch] if x != '')
+        return "SpecMask <{}".format(self.op.join(self.specs))
