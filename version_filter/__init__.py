80da10b262efacdcf4b4177e5a7ace7e8ffe3bbe
Paul Ortman <paul.ortman@gmail.com>
2017-05-20 13:02:07 -0500

    fix broken __init__.py

--- version_filter/__init__.py
+++ version_filter/__init__.py
@@ -4,4 +4,4 @@ import pkg_resources
 
 __author__ = """Dropseed"""
 __email__ = 'python@dropseed.io'
-__version__ = pkg_resources.get_distribution("configyaml").version
+__version__ = pkg_resources.get_distribution("version_filter").version
